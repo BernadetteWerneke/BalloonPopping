93b9a5d665d8ef597534176d89471545c2ca43c6
Bernadette Werneke <bernadettewerneke@gmail.com>
2024-08-09 16:12:30 +0200

    creating timer

--- app/src/main/java/com/appperfektion/balloonpopping/MainActivity.kt
+++ app/src/main/java/com/appperfektion/balloonpopping/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.appperfektion.balloonpopping
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.appperfektion.balloonpopping.databinding.ActivityMainBinding
 
@@ -13,5 +16,29 @@ class MainActivity : AppCompatActivity() {
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
 
+        object : CountDownTimer(5000, 1000){
+            override fun onTick(millisUntilFinished: Long) {
+                mainBinding.textViewTimer.text = (millisUntilFinished / 1000).toString()     //time in seconds, type long
+            }
+
+            override fun onFinish() {
+                mainBinding.textViewTimer.visibility = View.INVISIBLE
+                mainBinding.textViewRemainingTime.visibility = View.VISIBLE
+                mainBinding.textViewScore.visibility = View.VISIBLE
+                mainBinding.gridLayout.visibility = View.VISIBLE
+
+                object : CountDownTimer(30000, 1000){
+                    override fun onTick(millisUntilFinished: Long) {
+                        mainBinding.textViewRemainingTime.text = "Remaining Time: ${millisUntilFinished/1000}"
+                    }
+
+                    override fun onFinish() {
+                        val intent = Intent(this@MainActivity,ResultActivity::class.java)
+                        startActivity(intent)
+                    }
+                }.start()
+            }
+
+        }.start()
     }
 }
