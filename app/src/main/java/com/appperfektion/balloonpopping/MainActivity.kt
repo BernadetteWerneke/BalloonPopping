b094a35fb2ad67645422ecb361dfef1330e6d8c4
Bernadette Werneke <bernadettewerneke@gmail.com>
2024-08-11 07:55:43 +0200

    moving the ballon

--- app/src/main/java/com/appperfektion/balloonpopping/MainActivity.kt
+++ app/src/main/java/com/appperfektion/balloonpopping/MainActivity.kt
@@ -3,19 +3,38 @@ package com.appperfektion.balloonpopping
 import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.os.Handler
+import android.os.Looper
 import android.view.View
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.appperfektion.balloonpopping.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var mainBinding: ActivityMainBinding
     var score = 0
+    lateinit var handler: Handler
+    lateinit var runnable: Runnable
+    lateinit var balloonsArray: Array<ImageView>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
+
+        balloonsArray = arrayOf(
+            mainBinding.balloon1,
+            mainBinding.balloon2,
+            mainBinding.balloon3,
+            mainBinding.balloon4,
+            mainBinding.balloon5,
+            mainBinding.balloon6,
+            mainBinding.balloon7,
+            mainBinding.balloon8,
+            mainBinding.balloon9
+        )
 
         object : CountDownTimer(5000, 1000){
             override fun onTick(millisUntilFinished: Long) {
@@ -23,10 +42,8 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
-                mainBinding.textViewTimer.visibility = View.INVISIBLE
-                mainBinding.textViewRemainingTime.visibility = View.VISIBLE
-                mainBinding.textViewScore.visibility = View.VISIBLE
-                mainBinding.gridLayout.visibility = View.VISIBLE
+
+                balloonControl()
 
                 object : CountDownTimer(30000, 1000){
                     override fun onTick(millisUntilFinished: Long) {
@@ -41,6 +58,26 @@ class MainActivity : AppCompatActivity() {
             }
 
         }.start()
+    }
+
+    private fun balloonControl() {
+        mainBinding.textViewTimer.visibility = View.INVISIBLE
+        mainBinding.textViewRemainingTime.visibility = View.VISIBLE
+        mainBinding.textViewScore.visibility = View.VISIBLE
+
+        handler = Handler(Looper.getMainLooper())
+        runnable = Runnable{
+
+            for (balloon in balloonsArray){
+                   balloon.visibility =  View.INVISIBLE
+            }
+            mainBinding.gridLayout.visibility = View.VISIBLE
+            val i = Random.nextInt(balloonsArray.size)
+            balloonsArray[i].visibility = View.VISIBLE
+
+            handler.postDelayed(runnable, 2000)
+        }
+        handler.post(runnable)
     }
 
     fun increaseScoreByOne(view: View){
